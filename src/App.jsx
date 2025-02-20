22ca96c5832047a00ea2075d52339e83bf440a43
ChaofanMa <chaofanmah@gmail.com>
2025-02-20 13:00:48 -0500

    set up folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.scss";
+import { useState } from 'react'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <p>this is cat care app</p>
+
+      <p >
+        This is cat care
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
